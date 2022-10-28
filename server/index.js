345cf01f08f2018e6e8096070651df689343ea9f
Georgi <figoto660@gmail.com>
2022-10-27 22:51:24 -0400

    adding express and mongodb

--- server/index.js
+++ server/index.js
@@ -3,3 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb" , extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb" , extended: true}));
+app.use(cors());
+
+const CONNECTIO_URL = "mongodb+srv://figoto660:<password>@cluster0.y8hc2ck.mongodb.net/?retryWrites=true&w=majority";
